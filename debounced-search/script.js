cf09fa65c3515c6b631e964cbf49c6aac8aa1129
vengadeshraj <vengadeshraj405@gmail.com>
2025-11-17 20:42:12 +0530

    debouncing logic complete

--- debounced-search/script.js
+++ debounced-search/script.js
@@ -1,5 +1,52 @@
-const searchBox = document.getElementById("search-box");
+const FRUITS = [
+  "Apple",
+  "Banana",
+  "Orange",
+  "Mango",
+  "Pineapple",
+  "Grapes",
+  "Watermelon",
+  "Strawberry",
+  "Papaya",
+  "Guava",
+  "Kiwi",
+  "Blueberry",
+  "Cherry",
+  "Peach",
+  "Pear",
+  "Lemon",
+  "Fig",
+  "Pomegranate",
+  "Lychee",
+  "Dragonfruit",
+];
 
-searchBox.addEventListener("input", function () {
-  console.log("ett");
+const DELAY = 300;
+
+const searchBox = document.getElementById("search-box");
+const suggestionsList = document.getElementById("suggested-fruits");
+let timerId;
+
+searchBox.addEventListener("input", function (e) {
+  const input = e.target.value;
+  suggestionsList.innerHTML = "";
+  if (timerId) clearTimeout(timerId);
+  
+
+  if (input) {
+    timerId = setTimeout(() => {
+
+      const suggestions = FRUITS.filter((fruit) => {
+        if (fruit.toLowerCase().startsWith(input.toLowerCase())) return fruit;
+      });
+
+      suggestions.forEach((fruit) => {
+        const li = document.createElement("li");
+        li.textContent = fruit;
+        suggestionsList.appendChild(li);
+      });
+
+      timerId = null;
+    }, DELAY);
+  }
 });
